87473fbdcc2e652c26d68e4b0fc6c574641606a4
sarahramis <95698061+sarahramis@users.noreply.github.com>
2021-12-19 14:09:08 +0000

    Numpy nn: added softmax / crossentropy loss
    Numpy nn: added softmax / crossentropy loss

--- nn.py
+++ nn.py
@@ -4,13 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
+
 def relu(x):
     # https://numpy.org/doc/stable/reference/generated/numpy.where.html
     return np.where(x>0, x, 0)
 
+
 # https://numpy.org/doc/stable/reference/generated/numpy.reciprocal.html
 def sigmoid(x):
-     return np.reciprocal(1+np.exp(-1*x))
+    return np.reciprocal(1+np.exp(-1*x))
+
+
+# https://machinelearningmastery.com/softmax-activation-function-with-python/
+def softmax(x):
+    exp_x = np.exp(x)
+    return exp_x/exp_x.sum()
+
+
+# https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
+# https://realpython.com/gradient-descent-algorithm-python/
+def crossentropy_loss(true_y,pred_y):
+    part1 = np.matmul(np.log(1 - pred_y), (1 - true_y))
+    part2 = np.matmul(true_y, np.log(pred_y))
+    return -1*(part1.sum() + part2.sum())
+
+
+
+
 
 # https://www.tensorflow.org/tutorials/keras/classification
 fashion_mnist = tf.keras.datasets.fashion_mnist
@@ -40,8 +60,12 @@ with open('mnist_data.pickle', 'rb') as f:
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-vector = np.random.random([10,])-0.5
-print(vector)
-print(relu(vector))
-print(sigmoid(vector))
+y_true = np.random.random([10,])
+y_pred = softmax(np.random.random([10,]))
+# https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.round_.html
+
+y_true = np.round(y_true,0)
+print(y_true)
+print(y_pred)
+print(crossentropy_loss(y_true, y_pred))
 
