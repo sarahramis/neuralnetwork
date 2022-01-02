abc73f65e34bffb6116db3c6f450f584423d492f
sarahramis <95698061+sarahramis@users.noreply.github.com>
2022-01-02 09:51:42 +0000

    Activation functions, debugged download
    This has the activation functions and more of the __init__ plus there was a bug in downloads.

--- nn.py
+++ nn.py
@@ -3,7 +3,9 @@ import numpy as np
 import tensorflow as tf
 import pickle
 
+REDOWNLOAD = False
 np.random.seed(26)
+
 # needs to be run with redownload set to True the first time
 # this downloads mnist using tensorflow
 # as saves it as a python pickle file
@@ -11,6 +13,7 @@ np.random.seed(26)
 # and it will just load it from local file
 def get_mnist(redownload=False):
     if redownload:
+        print("Redownloading...")
         # https://www.tensorflow.org/tutorials/keras/classification
         fashion_mnist = tf.keras.datasets.fashion_mnist
         # The below stops an error due to lack of ssl (it's
@@ -26,11 +29,12 @@ def get_mnist(redownload=False):
             # Handle target environment that doesn't support HTTPS verification
             ssl._create_default_https_context = _create_unverified_https_context
         # download
-        (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+        (X_train, y_train), (X_test, y_test) = fashion_mnist.load_data()
         # store on local disk
         # https://ianlondon.github.io/blog/pickling-basics/
         with open('mnist_data.pickle', 'wb') as f:
-            pickle.dump((train_images, train_labels,test_images, test_labels), f)
+            pickle.dump((X_train, y_train, X_test, y_test), f)
+
     else:
        # load the MNIST fashion from local disk (faster for development)
         with open('mnist_data.pickle', 'rb') as f:
@@ -43,19 +47,50 @@ def get_mnist(redownload=False):
 
 
 
-x_train, y_train, x_val, y_val, class_names = get_mnist()
-#print(x_train.shape)
-x_train = np.reshape(x_train, (x_train.shape[0], 784))
-x_val = np.reshape(x_val, (x_val.shape[0], 784))
-#print(x_train.shape)
+X_train, y_train, X_test, y_test, class_names = get_mnist(redownload=REDOWNLOAD)
+#print(X_train.shape)
+X_train = np.reshape(X_train, (X_train.shape[0], 784))
+X_test = np.reshape(X_test, (X_test.shape[0], 784))
+#print(X_train.shape)
 
-x_train, x_val = (x_train / 255).astype('float32'), (x_val / 255).astype('float32')
-y_train, y_val = to_categorical(y_train), to_categorical(y_val)
+X_train, X_test = (X_train / 255).astype('float32'), (X_test / 255).astype('float32')
+y_train, y_test = to_categorical(y_train), to_categorical(y_test)
+
 
 class NumpyNeuralNet():
-    def __init__(self, layers):
+    def __init__(self, layers, use_sigmoid=False):
+        if use_sigmoid:
+            self.activation_function = self.sigmoid
+            self.activation_function_back = self.sigmoid_backprop
+        else:
+            self.activation_function = self.relu
+            self.activation_function_back = self.relu_backprop
+
         self.layers = layers
-        # TO DO
+
+    # https://numpy.org/doc/stable/reference/generated/numpy.where.html
+    # https://stackoverflow.com/questions/46411180/implement-relu-derivative-in-python-numpy
+    def relu(self, x):
+        return np.where(x > 0, x, 0)
+
+    def relu_backprop(self, x):
+        x[x <= 0] = 0
+        x[x > 0] = 1
+        return x
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_backprop(self, x):
+        return (np.exp(-x)) / ((np.exp(-x) + 1) ** 2)
+
+    def softmax(self, x):
+        exp_x = np.exp(x - x.max())
+        return exp_x / np.sum(exp_x, axis=0)
+
+    def softmax_backprop(self, x):
+        exp_x = np.exp(x - x.max())
+        return exp_x / np.sum(exp_x, axis=0) * (1 - exp_x / np.sum(exp_x, axis=0))
 
     def activation(self, input_values):
         # TO DO
@@ -70,31 +105,5 @@ class NumpyNeuralNet():
         pass
 
 
-    def relu(x):
-        # https://numpy.org/doc/stable/reference/generated/numpy.where.html
-        return np.where(x > 0, x, 0)
-
-    # https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
-    def sigmoid_backprop(dA, x):
-        sigmoid_x = sigmoid(x)
-        # chain rule result
-        return dA * sigmoid_x * (1 - sigmoid_x)
-
-    # https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
-    def relu_backprop(dA, Z):
-        dZ = np.array(dA, copy=True)
-        dZ[Z <= 0] = 0;
-        return dZ
-
-    # https://numpy.org/doc/stable/reference/generated/numpy.reciprocal.html
-    def sigmoid(x):
-        return np.reciprocal(1 + np.exp(-1 * x))
-
-    # https://machinelearningmastery.com/softmax-activation-function-with-python/
-    def softmax(x):
-        exp_x = np.exp(x)
-        return exp_x / exp_x.sum()
-
-
-neural_net = NumpyNeuralNet([784, 64, 32, 10])
+neural_net = NumpyNeuralNet(layers=[784, 64, 10], use_sigmoid=True)
 
