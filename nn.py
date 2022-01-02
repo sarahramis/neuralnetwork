45eb58882254af7a3e53da5ebf6dd2b46fc77183
sarahramis <95698061+sarahramis@users.noreply.github.com>
2022-01-02 01:06:41 +0000

    Re-boot of NN code, new preprocessing
    Preprocessing completed, works for MNIST-fashion instead of single output. Could not get my prev NN code detail to work when reconfigurable. So designing new functions. Checking structure in clean.

--- nn.py
+++ nn.py
@@ -1,159 +1,100 @@
-import random
-
-import tensorflow as tf
-
+from keras.utils.np_utils import to_categorical
 import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
 import pickle
 
-
-def same(x):
-    return x
-
-
-def relu(x):
-    # https://numpy.org/doc/stable/reference/generated/numpy.where.html
-    return np.where(x>0, x, 0)
-
-
-# https://numpy.org/doc/stable/reference/generated/numpy.reciprocal.html
-def sigmoid(x):
-    return np.reciprocal(1+np.exp(-1*x))
-
-
-# https://machinelearningmastery.com/softmax-activation-function-with-python/
-def softmax(x):
-    exp_x = np.exp(x)
-    return exp_x/exp_x.sum()
-
-
-# https://numpy.org/doc/stable/reference/generated/numpy.matmul.html
-# https://realpython.com/gradient-descent-algorithm-python/
-def crossentropy_loss(true_y,pred_y):
-    part1 = np.matmul(np.log(1 - pred_y), (1 - true_y))
-    part2 = np.matmul(true_y, np.log(pred_y))
-    return -1*(part1.sum() + part2.sum())
-
-
-class Layer:
-    def __init__(self, num_inputs, num_neurons,
-                 activation_function=relu):
-        self.num_neurons = num_neurons
-        self.num_inputs = num_inputs
-        # each input has a weight going to each neuron
-        self.weights = np.random.random(
-            [num_inputs,num_neurons])
-        self.bias = np.random.random([num_neurons,1])
-        self.activation_function = activation_function
-
-    def activation(self, input_values):
-        #print(self.weights.shape)
-        #print(input_values.shape)
-        z = np.sum(np.matmul(np.transpose(self.weights),
-                      input_values))
-        return self.activation_function(z+self.bias)
-
-    def __str__(self):
-        return f"Layer of {self.num_neurons} neurons, each with {self.num_inputs} inputs, giving {self.num_inputs*self.num_neurons} weights."
+np.random.seed(26)
+# needs to be run with redownload set to True the first time
+# this downloads mnist using tensorflow
+# as saves it as a python pickle file
+# then next time code is run, can set redownload to false
+# and it will just load it from local file
+def get_mnist(redownload=False):
+    if redownload:
+        # https://www.tensorflow.org/tutorials/keras/classification
+        fashion_mnist = tf.keras.datasets.fashion_mnist
+        # The below stops an error due to lack of ssl (it's
+        # to do with downloading via https
+        # https://github.com/tensorflow/tensorflow/issues/33285
+        import ssl
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        except AttributeError:
+            # Legacy Python that doesn't verify HTTPS certificates by default
+            pass
+        else:
+            # Handle target environment that doesn't support HTTPS verification
+            ssl._create_default_https_context = _create_unverified_https_context
+        # download
+        (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+        # store on local disk
+        # https://ianlondon.github.io/blog/pickling-basics/
+        with open('mnist_data.pickle', 'wb') as f:
+            pickle.dump((train_images, train_labels,test_images, test_labels), f)
+    else:
+       # load the MNIST fashion from local disk (faster for development)
+        with open('mnist_data.pickle', 'rb') as f:
+            X_train, y_train, X_test, y_test = pickle.load(f)
+    # tf doesn't provide the class names
+    # https://www.tensorflow.org/tutorials/keras/classification
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                      'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    return X_train, y_train, X_test, y_test, class_names
 
 
-class NeuralNetwork:
-    def __init__(self, layer_sizes, layer_activations):
-        layers = []
-        # assume first layer is an input layer
-        for i, layer_size in enumerate(layer_sizes):
-            if i > 0:
-                # num inputs is number of neurons in
-                # previous layer
-                num_inputs = layer_sizes[i-1]
-                print(f"num_inputs in initialisation {num_inputs}")
-                # num_neurons defined by user
-                num_neurons = layer_size
-                layers.append(Layer(
-                    num_inputs=num_inputs,
-                    num_neurons=num_neurons,
-                    activation_function=layer_activations[i]))
-            else:  #is an input layer
-                num_inputs = layer_size
-                num_neurons = layer_size
-                input_layer = Layer(
-                    num_inputs=num_inputs,
-                    num_neurons=num_neurons,
-                    activation_function=layer_activations[i])
-                # no weights on inputs
-                input_layer.weights = np.array([])
-                layers.append(input_layer)
+
+x_train, y_train, x_val, y_val, class_names = get_mnist()
+#print(x_train.shape)
+x_train = np.reshape(x_train, (x_train.shape[0], 784))
+x_val = np.reshape(x_val, (x_val.shape[0], 784))
+#print(x_train.shape)
+
+x_train, x_val = (x_train / 255).astype('float32'), (x_val / 255).astype('float32')
+y_train, y_val = to_categorical(y_train), to_categorical(y_val)
+
+class NumpyNeuralNet():
+    def __init__(self, layers):
         self.layers = layers
+        # TO DO
 
     def activation(self, input_values):
-        self.activation_values = []    # store for backprop
-        for i, layer in enumerate(self.layers):
-            print(f"Layer {i} input shape {input_values.shape} ")
-            print(f"input {input_values}")
-            if i > 0:
-                input_values = layer.activation(input_values)
-            else:
-                input_values = input_values
-            self.activation_values.append(input_values)
-            print(f"output {input_values}")
-            print(f"output shape {input_values.shape}")
-            print("----")
-        return input_values
+        # TO DO
+        pass
 
-    def __str__(self):
-        final_string = "Neural Network:\n"
-        for layer in self.layers:
-            final_string += str(layer) + "\n"
-        return final_string
+    def activation_backprop(self, output_values, correct_outputs):
+        # TO DO
+        pass
+
+    def train(self, X, y):
+        # TO DO
+        pass
 
 
+    def relu(x):
+        # https://numpy.org/doc/stable/reference/generated/numpy.where.html
+        return np.where(x > 0, x, 0)
+
+    # https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
+    def sigmoid_backprop(dA, x):
+        sigmoid_x = sigmoid(x)
+        # chain rule result
+        return dA * sigmoid_x * (1 - sigmoid_x)
+
+    # https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
+    def relu_backprop(dA, Z):
+        dZ = np.array(dA, copy=True)
+        dZ[Z <= 0] = 0;
+        return dZ
+
+    # https://numpy.org/doc/stable/reference/generated/numpy.reciprocal.html
+    def sigmoid(x):
+        return np.reciprocal(1 + np.exp(-1 * x))
+
+    # https://machinelearningmastery.com/softmax-activation-function-with-python/
+    def softmax(x):
+        exp_x = np.exp(x)
+        return exp_x / exp_x.sum()
 
 
-# https://www.tensorflow.org/tutorials/keras/classification
-fashion_mnist = tf.keras.datasets.fashion_mnist
+neural_net = NumpyNeuralNet([784, 64, 32, 10])
 
-# https://github.com/tensorflow/tensorflow/issues/33285
-import ssl
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
-
-# https://www.tensorflow.org/tutorials/keras/classification
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-#(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-
-#https://ianlondon.github.io/blog/pickling-basics/
-#with open('mnist_data.pickle', 'wb') as f:
-#    pickle.dump((train_images, train_labels,test_images, test_labels), f)
-with open('mnist_data.pickle', 'rb') as f:
-    train_images, train_labels,test_images, test_labels = pickle.load(f)
-
-train_images = train_images / 255.0
-test_images = test_images / 255.0
-
-y_true = np.random.random([10,])
-y_pred = softmax(np.random.random([10,]))
-# https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.round_.html
-
-"""y_true = np.round(y_true,0)
-print(y_true)
-print(y_pred)
-print(crossentropy_loss(y_true, y_pred))"""
-
-L = Layer(2, 1, activation_function=relu)
-L.weights[0] = 1
-L.weights[1] = -0.5
-L.bias = 0
-inputs = np.array([2,1])
-print(L.activation(inputs))
-print(L)
-NN = NeuralNetwork([4, 8, 1], [same, relu, sigmoid])
-print(NN)
-input = np.array([-1,1,-1,1])
-print(NN.activation(input))
